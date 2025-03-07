fca1fb1825cd3c561c63ba1e8fd1e50cf5b09892
snailGB <lgb8572@gmail.com>
2025-03-07 13:39:43 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2568 ms, Memory: 346420 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		
+		int N = Integer.valueOf(br.readLine());
+		
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.valueOf(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i : arr)
+			bw.write(i + "\n");
+		
+		bw.flush();
+	}
+}
