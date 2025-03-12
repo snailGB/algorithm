af9d78984ebc8adfd777a87239ef1b59b62814f1
snailGB <lgb8572@gmail.com>
2025-03-12 13:19:37 +0900

    [Gold V] Title: 거의 소수, Time: 288 ms, Memory: 28572 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -1,48 +1,53 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
-        // from과 to를 입력받음
         long from = sc.nextLong();
         long to = sc.nextLong();
 
-        // to의 제곱근까지의 소수를 구하기 위한 배열
         int max = (int) Math.sqrt(to);
         boolean[] isPrime = new boolean[max + 1];
-        
-        // 에라토스테네스의 체로 소수 구하기
-        for (int i = 2; i <= max; i++) {
-            isPrime[i] = true;
+
+        for (int num = 2; num <= max; num++) {
+            isPrime[num] = true;
         }
 
-        for (int i = 2; i <= Math.sqrt(max); i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= max; j += i) {
-                    isPrime[j] = false;
-                }
+        for (int stdNum = 2; stdNum <= Math.sqrt(max); stdNum++) {
+            if (!isPrime[stdNum]) {
+                continue;
+            }
+
+            for (int multiple = stdNum * 2; multiple <= max; multiple += stdNum) {
+                isPrime[multiple] = false;
             }
         }
 
-        int count = 0;
-        
-        // 소수의 거듭제곱 수 찾기
-        for (int i = 2; i <= max; i++) {
-            if (isPrime[i]) {
-                long power = (long) i * i; // p^2 부터 시작
-                while (power <= to) {
-                    if (power >= from) {
-                        count++;
-                    }
-                    // 오버플로우 방지
-                    if (power > to / i) break;
-                    power *= i;
+        int answer = 0;
+
+        for (int num = 2; num <= max; num++) {
+            if (!isPrime[num]) {
+                continue;
+            }
+
+            long square = (long) num * num;
+            while (square <= to) {
+                if (square >= from) {
+                    answer++;
                 }
+
+                if (square > to / num) {
+                    break;
+                }
+
+                square *= num;
             }
         }
 
-        // 결과 출력
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
